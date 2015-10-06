147d003f43e0385c41fe9968a78269aa26afea31
Kyle Petrovich <kylepetrovich@gmail.com>
2015-10-06 15:50:08 -0400

    indexing page controller

--- PageViewController.swift
+++ PageViewController.swift
@@ -12,32 +12,40 @@ import UIKit
 
 class PageViewController : UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
     
-    var currentIndex = 0
-    let MAX_INDEX = 2
+    let INDICES = [0,1,2]
     
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 3
+    }
     
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
 
     override func viewDidLoad() {
         self.dataSource = self
-        let contentController = self.getStagesViewControllerAtIndex(currentIndex)
+        let contentController = self.getStagesViewControllerAtIndex(0)
         self.setViewControllers([contentController!], direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: nil)
     }
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-        if (currentIndex + 1 > MAX_INDEX) {
+        let vc = viewController as! ViewController
+        if (vc.PageIndex >= INDICES.count - 1) {
             return nil
         } else {
-            return getStagesViewControllerAtIndex(currentIndex + 1)
+            return getStagesViewControllerAtIndex(vc.PageIndex + 1)
         }
     }
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-        if (currentIndex - 1 < 0) {
+        let vc = viewController as! ViewController
+        if (vc.PageIndex <= 0) {
             return nil
         } else {
-            return getStagesViewControllerAtIndex(currentIndex - 1)
+            return getStagesViewControllerAtIndex(vc.PageIndex - 1)
         }
     }
+    
     func getStagesViewControllerAtIndex(index: Int) -> UIViewController? {
         let stageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ViewController") as! ViewController
         stageViewController.PageIndex = index
