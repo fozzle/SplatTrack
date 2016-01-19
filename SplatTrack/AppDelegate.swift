bec58a74418165ef20305e5b25bd07b9d34f88fe
Kyle Petrovich <kylepetrovich@gmail.com>
2016-01-19 17:08:09 -0500

    readd app delegate without keys

---
+++ SplatTrack/AppDelegate.swift
@@ -0,0 +1,90 @@
+//
+//  AppDelegate.swift
+//  SplatTrack
+//
+//  Created by Kyle Petrovich on 8/31/15.
+//  Copyright (c) 2015 Kyle Petrovich. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // Initialize Parse.
+        Parse.setApplicationId(valueForAPIKey("PARSE_APP_ID"),
+            clientKey: valueForAPIKey("PARSE_CLIENT_KEY"))
+        
+        
+        // Notif work
+        NotificationManager.registerForNotifications()
+        
+        clearNotifs()
+        
+        
+        UIApplication.sharedApplication().registerForRemoteNotifications()
+        
+        // Theme navbar
+        if let font = UIFont(name: "Project Paintball", size: 30.0) {
+            UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: font]
+        }
+        
+        return true
+    }
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        // Store the deviceToken in the current installation and save it to Parse.
+        let currentInstallation = PFInstallation.currentInstallation();
+        currentInstallation.setDeviceTokenFromData(deviceToken);
+        currentInstallation.saveInBackground();
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        print("fuck")
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: GlobalConstants.SplatStageChange, object: nil))
+        PFPush.handlePush(userInfo)
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        clearNotifs()
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func clearNotifs() {
+        // Safety for old style local notifs
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        
+        // Notifs only clear if badge # changes, so hack to force clear
+        UIApplication.sharedApplication().applicationIconBadgeNumber = 1
+        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
+
+    }
+
+
+}
