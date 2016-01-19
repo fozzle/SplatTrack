854797acf4b18c280e6a6eb4965db7219899ae98
Kyle Petrovich <kylepetrovich@gmail.com>
2016-01-19 17:43:23 -0500

    remove commented

--- SplatTrack/Views/StageCardView.swift
+++ SplatTrack/Views/StageCardView.swift
@@ -56,16 +56,5 @@ class StageCardView: UIView {
         self.embeddedView.frame = self.bounds
         self.embeddedView.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
     }
-    
-    
-    
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
 
 }
